22da0810a862a95568c54116e65279101f5c5cea
Nwabueze Franklin <99493414+NwabuezeFranklin@users.noreply.github.com>
2022-10-28 04:10:10 +0100

    React Pagination

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState} from "react";
 import ReactPaginate from "react-paginate";
-import '/src/App.css'
+import '/src/App.css';
 
 export default function images(props) {
     const { results } = props;
